97299d9febdc484f8d158d3ae272093950055ce0
SadikMukradir <sadikmuktadir2.0@gmail.com>
2025-01-31 22:35:16 +0600

    dark mode position updated

--- src/components/home/rightSection/RightSection.tsx
+++ src/components/home/rightSection/RightSection.tsx
@@ -7,7 +7,7 @@ import Services from "./Services";
 const RightSection = () => {
   return (
     <div>
-      <div className="fixed top-0 left-0">
+      <div className="">
         <DarkMode></DarkMode>
       </div>
       <div id="home">
